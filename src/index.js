6aef179b3290fbea07ac994ddf8c055bbb9b0718
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-03-05 23:11:21 +0200

    feat: add redux devTools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { createStore } from 'redux'
-import { Provide, Provider } from 'react-redux'
-import rootReducer from './redux/rootReducer'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
 
-const store = createStore(rootReducer)
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware()))
 
 const app = (
   <Provider store={store}>
--- src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-const initialState = {
-  count: 0,
-}
-
-export default function rootReducer(state = initialState, action) {
-  return state
-}
