ebc801fface0df181fcff6800cce3ed71da4f466
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-03-17 23:45:53 +0200

    feat: add localstorage

--- src/index.js
+++ src/index.js
@@ -7,14 +7,30 @@ import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import rootReducer from './store/reducers/rootReducer'
+import thtottlr from 'lodash.throttle'
+import { loadState, saveState } from './localStorage'
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose
+const presistedState = loadState()
+const store = createStore(
+  rootReducer,
+  presistedState,
+  composeEnhancers(applyMiddleware(thunk))
+)
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
-
+store.subscribe(
+  thtottlr(() => {
+    saveState({
+      sort: store.getState().sort,
+      search: store.getState().search,
+      check: store.getState().check,
+      enum: store.getState().enum,
+    })
+  }, 1000)
+)
 const app = (
   <Provider store={store}>
     <App />
